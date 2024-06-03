a2c00b0ee06e570d7a61b8eaf247c65fbe55a828
iMatt <115056575+themattery@users.noreply.github.com>
2024-06-02 22:54:42 -0300

    day 16

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,21 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+while is_on:
+    order = input(f"What would you like? ({menu.get_items()}): ")
+    if order == 'off':
+        is_on = False
+    elif order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink):
+           if money_machine.make_payment(drink.cost):
+               coffee_maker.make_coffee(drink)
