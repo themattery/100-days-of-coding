d636dd04df1808484899ce534a77310389098d54
iMatt <115056575+themattery@users.noreply.github.com>
2024-06-05 15:43:19 -0300

    day 4

--- Day #004/treasure_map.py
+++ Day #004/treasure_map.py
@@ -3,7 +3,7 @@ line2 = ["⬜️","⬜️","️⬜️"]
 line3 = ["⬜️️","⬜️️","⬜️️"]
 map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
-position = input().upper() # Where do you want to put the treasure?
+position = input().upper()
 
 column = position[0]
 line = position[1]
