d161ef64614349c6770ce623fca749ee5b0dfb76
iMatt <115056575+themattery@users.noreply.github.com>
2024-06-07 21:51:53 -0300

    day 6

--- Day #006/reeborg_maze.py
+++ Day #006/reeborg_maze.py
@@ -1,5 +1,4 @@
 # From Reeborg's World -> https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
-
 # Our robot takes the right wall as a guide to the flag :)
 
 def turn_right():
@@ -15,5 +14,3 @@ while not at_goal():
         move()
     else:
         turn_left()
-
-# Note -> fix code's exceptions on day 15
